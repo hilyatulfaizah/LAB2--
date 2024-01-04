9a917908a7cb09a47945eb4312822cf3874b34df
zarithLab1 <2022496474@student.uitm.edu.my>
2024-01-04 22:32:49 +0800

    zarith commit

--- app/src/main/java/com/example/lab2_hilyatul/MainActivity.java
+++ app/src/main/java/com/example/lab2_hilyatul/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-
+// testingggg
 public class MainActivity extends AppCompatActivity {
 
     @Override
